76bb7e662a0f3cf072a8c66138932b2f1102e1ac
Nitish Kr <pnnitish88725@gmail.com>
2025-10-30 17:37:16 +0530

    add frontend link

--- server.js
+++ server.js
@@ -11,12 +11,24 @@ import { validateRegister,validateLogin } from "./validators/authValidator.js";
 dotenv.config();
 
 const app = express();
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:3000",
+  "https://pn-dsa-visuliazer.vercel.app"
+];
+
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://pn-dsa-visuliazer.vercel.app/"
-  ],
-  credentials: true
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      console.warn("Blocked CORS for origin:", origin);
+      return callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
 }));
 app.use(bodyParser.json());
 
